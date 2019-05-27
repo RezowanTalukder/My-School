ef1e97599630942837c24409edd86516427c79c5
nazifaHia <35560210+nazifaHia@users.noreply.github.com>
2019-05-27 23:33:33 +0600

    Add files via upload

--- teacher/assUpload.php
+++ teacher/assUpload.php
@@ -5,20 +5,22 @@ include 'dbconfig.php';
 $statusMsg = '';
 
 // File upload path
-$targetDir = "uploadedNotes/";
+$targetDir = "asFolder/";
 $fileName = basename($_FILES["file"]["name"]);
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
 
 if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
 
-    $allowTypes = array('jpg','png','jpeg','gif','pdf','zip','ppt','rar','doc','docx','txt');
+    $allowTypes = array('jpg','png','jpeg','gif','pdf','zip','rar','doc','docx','txt');
+
+    $name = _POST["ass_name"] ;
 
     if(in_array($fileType, $allowTypes)){
 
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
 
-            $insert = $db->query("INSERT into tbl_note (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+            $insert = $db->query("INSERT into tbl_class (name, assignment,uploaded_on) VALUES ('".$fileName."', '".$ass_name."' , NOW())");
             if($insert){
                 $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
             }else{
--- teacher/classroom.php
+++ teacher/classroom.php
@@ -6,11 +6,17 @@
 
 <div class="main">
 <h1 style="text-align: center">Teacher</h1>
-    <div class="adminpanel">
-        <h2 style="text-align: center">Create Your Classroom</h2>
-        <button>
-        	
-        </button>
+    <div class="adminpanel" style="background-color:  #003300">
+         <form action="upload.php" method="post" enctype="multipart/form-data">
+		    
+		    Assignment Name: 
+		    <input type="text" name="ass_name" id="ass_name"><br>
+
+		    <h3>Select File to Upload:</h3><br><br>
+		    <input type="file" name="file">
+		    <input type="submit" name="submit" value="Upload">
+		</form>
+
     </div>
 
 
